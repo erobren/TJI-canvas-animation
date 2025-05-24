fdf936fafd04a3f2d7f8095d18c1a9ee12cc3007
robert <robert.renbris@gmail.com>
2025-05-24 12:24:36 +0200

    Canvas animation setup and animating dots.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,80 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas");
+canvas.width = container.clientWidth;
+canvas.height = container.clientHeight;
 
-setupCounter(document.querySelector('#counter'))
+const context = canvas.getContext("2d");
+
+class Dot {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+
+    this.xMovement = 1;
+    this.yMovement = 1;
+
+    this.size = 10;
+  }
+
+  move() {
+    this.x += this.xMovement;
+    this.y += this.yMovement;
+  }
+
+  accellerate() {
+    if(this.x > canvas.width || this.x < 0) {
+      this.xMovement = -this.xMovement;
+    }
+    if(this.y > canvas.height || this.y < 0) {
+      this.yMovement = -this.yMovement;
+    }
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.size, 0, 2 * Math.PI, true);
+    context.fillStyle = 'white';
+    context.fill();
+  }
+}
+
+// const dot = new Dot();
+const dots = [...Array(50)].map(() => new Dot());
+
+function clearAndRender() {
+  context.fillStyle = "rgba(0, 0, 0, 0.1)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  
+  for(let dot of dots) {
+    dot.draw(context);
+    dot.move();
+    dot.accellerate();
+  }
+}
+
+// function renderLoop() {
+//   clearAndRender();
+//   setTimeout(renderLoop, 0);
+// }
+
+async function renderLoop() {
+  while(true) {
+    clearAndRender();
+    await releaseControl()
+  }
+}
+
+function releaseControl(waitTimeMs=0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, waitTimeMs)
+  })
+}
+
+renderLoop();
+
+
+
+
+
